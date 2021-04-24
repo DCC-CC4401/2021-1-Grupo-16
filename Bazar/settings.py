83f2f6c8051db7f61a122164b116485a268df11d
ppizarror <pablo@ppizarror.com>
2021-04-23 23:57:44 -0400

    Added basic bazar template structure

--- Bazar/settings.py
+++ Bazar/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Bazar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Bazar/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Bazar/views.py
+++ Bazar/views.py
@@ -5,10 +5,11 @@ Implements the main views of the app.
 """
 
 from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
 
 
 def view_home(request: 'HttpRequest') -> 'HttpResponse':
     """
     Home app.
     """
-    return HttpResponse('test')
+    return render(request, 'home.html', {})
