1e944788ec393a0f9371b3a27e9329ba70d86a08
Lomolisso <raul.delafuente@ing.uchile.cl>
2021-04-28 16:38:52 -0400

    created signup template

---
+++ Bazar/static/Bazar/signup/signup.js
@@ -0,0 +1,46 @@
+/**
+ SIGN UP
+ signup module.
+ */
+"use strict";
+
+/**
+ * signup module.
+ *
+ * @class
+ * @extends {Module}
+ */
+function SignUpModule() {
+
+    Module.call(this);
+
+    /**
+     * Object pointer
+     * @type {SignUpModule}
+     */
+    let self = this;
+
+    /**
+     * Inits the module.
+     */
+    this.init = function () {
+        app_console.info('Initializing signup module');
+
+        // Load DOM objects
+        let $contents = $('#contents');
+
+        // Init header
+        self.initBasicHeader('Sign Up');
+        self._header.registerToolPopupCloseElement($contents);
+
+        // Init footer
+        self.initBasicFooter();
+
+        // Set the contents max height
+        self.extendDivToMaxHeight($contents);
+
+    };
+
+}
+
+app_module.registerModule('signup', new SignUpModule());
