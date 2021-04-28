5d4d01b67bef0bfe2c34aad03fe39cc877479d19
ppizarror <pablo@ppizarror.com>
2021-04-28 12:37:54 -0400

    Added profile basic module

---
+++ Bazar/static/src/modules/user/profile.js
@@ -0,0 +1,56 @@
+/**
+ HOME
+ Home module.
+ */
+"use strict";
+
+/**
+ * Home module.
+ *
+ * @class
+ * @extends {Module}
+ */
+function HomeModule() {
+
+    Module.call(this);
+
+    /**
+     * Object pointer
+     * @type {HomeModule}
+     */
+    let self = this;
+
+    /**
+     * Search object
+     * @type {HomeModuleSearchComponent}
+     * @private
+     */
+    this._search = null;
+
+    /**
+     * Inits the module.
+     */
+    this.init = function () {
+        app_console.info('Initializing home module');
+
+        // Load DOM objects
+        let $contents = $('#contents');
+
+        // Init header
+        self.initBasicHeader('Bazar');
+        self._header.registerToolPopupCloseElement($contents);
+
+        self.initBasicFooter();
+
+        // Update contents height to fit window height
+        let $contents_height = self.extendDivToMaxHeight($contents);
+
+        // Setup search object
+        self._search = new HomeModuleSearchComponent();
+        self._search.init($contents_height);
+
+    };
+
+}
+
+app_module.registerModule('home', new HomeModule());
