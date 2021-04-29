c426d78563eaa97dddcc8dc903f04484060dd3cb
Lomolisso <raul.delafuente@ing.uchile.cl>
2021-04-29 13:57:31 -0400

    Sign up implemented

--- Bazar/views.py
+++ Bazar/views.py
@@ -4,10 +4,30 @@ Bazar
 Implements the main views of the app.
 """
 
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.utils import timezone
 from datetime import date
+from users.models import User
 
+def is_used(an_username: str) -> bool:
+    """
+    Checks in the User table if an_username is already in use.
+    """
+    if len(User.objects.filter(username=an_username))!=0:
+        return True
+    return False
+
+def generate_username(an_username: str) -> str:
+    """
+    Gets an_username and generates a_new_username that isn't
+    in use.
+    """
+    a_new_username = an_username
+    while(is_used(a_new_username)):
+        a_new_username = an_username + '_' + str(k)
+        k+=1 
+    return a_new_username
 
 def view_home(request: 'HttpRequest') -> 'HttpResponse':
     """
@@ -27,4 +47,20 @@ def view_signup(request: 'HttpRequest') -> 'HttpResponse':
     """
     Sign up.
     """
-    return render(request, 'Bazar/signup.html', {})
+    if request.method == "GET":
+        return render(request, 'Bazar/signup.html', {})
+    
+    elif request.method == "POST":
+        a_first_name = request.POST["first_name"]
+        a_last_name = request.POST["last_name"]
+        an_email = request.POST["email"]
+        a_password = request.POST["password"]
+        a_day = int(request.POST["day"])
+        a_month = int(request.POST["month"])
+        a_year = int(request.POST["year"])
+        a_gender = request.POST["gender"]
+        an_username = generate_username(an_email.split("@")[0])
+
+        # Adding the new user to the User table.
+        user = User.objects.create_user(first_name=a_first_name, last_name=a_last_name, email=an_email, password=a_password, username=an_username, birthday=date(a_year,a_month,a_day), date_of_creation=timezone.now())
+        return HttpResponseRedirect('/home/')
