7f530c01c8c252e1e4c9bcd4dedb353080c8db3a
doragon_ <lung.trabajos@gmail.com>
2021-04-30 18:44:58 -0400

    :added login/edit profile implementation v1

--- Bazar/settings.py
+++ Bazar/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import users
 import os
+
+AUTH_USER_MODEL = 'user.User'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +84,11 @@ WSGI_APPLICATION = 'Bazar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bazardb',
+        'NAME': 'bazardb1',
         'USER': 'postgres',
-        'PASSWORD': 'grupo16',
+        'PASSWORD': 'lung1234',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
--- users/views.py
+++ users/views.py
@@ -4,12 +4,39 @@ Profile
 Implements the main views of the app.
 """
 
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .models import UserAddress, User
+from .forms import UserUpdateForm, UserAddressUpdateForm
 
 
 def view_profile(request: 'HttpRequest') -> 'HttpResponse':
     """
-    Home.
+    Profile.
+    (ONLY AVAILABLE WITH LOGGED IN)
     """
-    return render(request, 'users/profile.html', {})
+    address = UserAddress.objects.get(user=request.user) #OBTENEMOS LA DATA DEL ADDRES DEL USUARIO
+    user_address = UserAddress.objects.filter(user_id=request.user)
+    if request.method == 'POST': #SI EL USER NOS MANDA SU EDIT
+        u_form = UserUpdateForm(request.POST, instance=request.user) 
+        address_form = UserAddressUpdateForm(data=request.POST, instance=address) 
+    
+        if u_form.is_valid() and address_form.is_valid(): #VALIDAMOS
+            u_form.save()#GUARDAMOS EN LA BASE DE DATOS
+            address_form.save()
+            return redirect('/home/') #REDIRIGIMOS #TENER UN PROFIEL WITHOUT EDIT PLS
+    else: 
+        u_form = UserUpdateForm(instance=request.user) #PRE-LLENAMOS LOS DATOS DEL USUARIO
+        address_form = UserAddressUpdateForm(instance=address)
+
+    context = {
+        'u_form':u_form,
+        'address_form': address_form,
+        'user_address': user_address, #AGREGAR MAS FORMULARIOS SEGUN REQUIERA             
+        }
+
+    return render(request, "users/profile.html", context)   
+        
+
+
+
