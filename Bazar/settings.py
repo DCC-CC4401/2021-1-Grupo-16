71ae52abc151dc70f5b3eabeefe5295c4caeb802
doragon_ <lung.trabajos@gmail.com>
2021-05-31 12:53:11 -0400

    edit profile feature added

--- Bazar/settings.py
+++ Bazar/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'stores',
-    'users'
+    'users',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,8 @@ from django.contrib.auth.forms import UserCreationForm #Ver esto para el registe
 class UserUpdateForm(forms.ModelForm): #ESTO ES PARA PODER CAMBIAR LA INFO DEL USUARIO
     class Meta:
         model = User    #TOMAMOS EL USER DEL MODELO
-        fields = ['first_name'] #LOS CAMPOS QUE QUEREMOS CAMBIAR 
+        fields = ['first_name', 'last_name', 'email', 'username'] #LOS CAMPOS QUE QUEREMOS CAMBIAR 
+        
 
 class UserAddressUpdateForm(forms.ModelForm):
     class Meta:
