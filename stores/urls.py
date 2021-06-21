da7b42d68f2c0b16e624cc6f78085f45ed3bf709
doragon_ <lung.trabajos@gmail.com>
2021-06-20 21:37:01 -0400

    HTML STORE FRONT CREATED

--- stores/urls.py
+++ stores/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from stores.views import *
+from django.contrib import admin
+from django.urls import include
+
+
 
 urlpatterns = [
-
+    path('store/', view_store, name="view_store"),
 ]
--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from datetime import date
+from stores.models import *
+
 # Create your views here.
+
+def view_store(request: 'HttpRequest') -> 'HttpResponse':
+    """
+    Store front.
+    """
+    
+    return render(request, 'stores/storefront.html', {})
+
