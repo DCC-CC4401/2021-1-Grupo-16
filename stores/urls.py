98bb8f1f51dc01339f1ec9350bb3ef9327113378
ppizarror <pablo@ppizarror.com>
2021-06-21 00:16:11 -0400

    Add basic search store url

--- stores/urls.py
+++ stores/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from stores.views import *
-from django.contrib import admin
-from django.urls import include
 
-path('store/', view_store, name="view_store"),
-path('sprofile/<int:store_index>/', view_sprofile, name="store_profile"),
-path('sprofile/edit/<int:store_index>/', edit_sprofile, name="store_profile"),
-path('sinventory/<int:store_index>/', view_sinventory, name ="store_inventory"),
-path('sinventory/addproduct/<int:store_index>/', add_product, name ="add_product")
+urlpatterns = [
+    path('store/', view_store, name='view_store'),
+    path('sprofile/<int:store_index>/', view_sprofile, name='store_profile'),
+    path('sprofile/edit/<int:store_index>/', edit_sprofile, name='store_profile'),
+    path('sinventory/<int:store_index>/', view_sinventory, name='store_inventory'),
+    path('sinventory/addproduct/<int:store_index>/', add_product, name='add_product'),
+    path('store/search/<str:query>', search_store, name='search_store')
+]
--- stores/views.py
+++ stores/views.py
@@ -1,17 +1,17 @@
 from stores.forms import *
 from django.shortcuts import redirect, render
 from users.models import *
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
-from django.utils import timezone
-from datetime import date
+from django.http import HttpResponse, HttpRequest
 from stores.models import *
+
 
 # Create your views here.
 def view_store(request: 'HttpRequest') -> 'HttpResponse':
     """
     Store front.
-    """  
+    """
     return render(request, 'stores/storefront.html', {})
+
 
 def view_sprofile(request: 'HttpRequest', store_index: 'int') -> 'HttpResponse':
     """
@@ -20,10 +20,11 @@ def view_sprofile(request: 'HttpRequest', store_index: 'int') -> 'HttpResponse':
     administration_info = Administration.objects.filter(user_id=request.user)
     a_store = administration_info[store_index].store
     context = {
-        'store':a_store,
-        'store_index':store_index
+        'store': a_store,
+        'store_index': store_index
     }
     return render(request, 'stores/store_profile.html', context)
+
 
 def edit_sprofile(request: 'HttpRequest', store_index: 'int') -> 'HttpResponse':
     """
@@ -32,24 +33,25 @@ def edit_sprofile(request: 'HttpRequest', store_index: 'int') -> 'HttpResponse':
     administration_info = Administration.objects.filter(user_id=request.user)
     a_store = administration_info[store_index].store
 
-    if request.method == "POST":
+    if request.method == 'POST':
         s_form = StoreForm(request.POST, instance=a_store)
-        a_region = request.POST["region"]
-        a_commune = request.POST["commune"]
+        a_region = request.POST['region']
+        a_commune = request.POST['commune']
         s_form.region = a_region
         s_form.commune = a_commune
-
         if s_form.is_valid():
             s_form.save()
-            return redirect("/sprofile/"+str(store_index)+"/")
+            return redirect('/sprofile/{0}/'.format(str(store_index)))
+
     else:
         s_form = StoreForm()
         context = {
-            'store':a_store,
-            'store_index':store_index,
+            'store': a_store,
+            'store_index': store_index,
             'store_form': s_form
         }
         return render(request, 'stores/edit_store_profile.html', context)
+
 
 def view_sinventory(request: 'HttpRequest', store_index: 'int') -> 'HttpResponse':
     administration_info = Administration.objects.filter(user_id=request.user)
@@ -58,30 +60,36 @@ def view_sinventory(request: 'HttpRequest', store_index: 'int') -> 'HttpResponse
     store_inventory = [i.product for i in inventory_instances]
 
     context = {
-        'store':a_store,
-        'store_index':store_index,
-        'store_inventory':store_inventory
+        'store': a_store,
+        'store_index': store_index,
+        'store_inventory': store_inventory
     }
     return render(request, 'stores/store_inventory.html', context)
 
-def add_product(request: 'HttpRequest', store_index: 'int') -> 'HttpResponse':
+
+def add_product(request: 'HttpRequest', store_index: int) -> 'HttpResponse':
     administration_info = Administration.objects.filter(user_id=request.user)
     a_store = administration_info[store_index].store
 
-    if request.method == "POST":
+    if request.method == 'POST':
         p_form = ProductForm(request.POST)
-        
+
         if p_form.is_valid():
             product = p_form.save()
             i = Inventory(store=a_store, product=product)
             i.save()
-            return redirect("/sinventory/"+str(store_index)+"/")
-        
+            return redirect('/sinventory/{0}/'.format(str(store_index)))
+
     else:
         p_form = ProductForm()
         context = {
-            'store':a_store,
-            'store_index':store_index,
-            'inventory_form':p_form
+            'store': a_store,
+            'store_index': store_index,
+            'inventory_form': p_form
         }
         return render(request, 'stores/add_product.html', context)
+
+
+def search_store(request: 'HttpRequest', query: str) -> 'HttpResponse':
+    # Search all stories
+    return HttpResponse()
